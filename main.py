9c8515f7eb58a71ebea7a3724872499ff89fedba
Davi <davirdavanzo@gmail.com>
2025-12-08 10:16:20 -0300

    Refatoring

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import crawler
 url_tree = dict()
 
 def main():
-    methods = [print, fuzzing, crawling, print_url_tree, print_params]
+    methods = [print, fuzzing, crawling, print_url_tree, print_params, print_urls_params]
 
     option = 99
     parser = argparse.ArgumentParser(description="ShingekiSuite")
@@ -28,6 +28,7 @@ def print_menu():
     print("2) Crawl")
     print("3) Show available URLs")
     print("4) Print params")
+    print("5) Print URLs and params")
     print("0) Exit")
 
 def fuzzing():
@@ -39,30 +40,44 @@ def crawling():
     target_url = search_tree(input("Target URL: "), url_tree)
     target_url["childs"] = crawler.crawl(target_url["url"])
 
-def print_url_tree(node=url_tree, number="1", level=1):
-    spaces = ""
+def print_url_tree(current_node=url_tree, node_number="1", level=1, include_params=False):
+    identation = ""
+
     for l in range(0, level):
-        spaces += " "
+        identation += "  "
 
-    print(f"{spaces}{number}. {node["url"]}")
+    print(f"{identation}{node_number}. {current_node["url"]}")
 
-    if("childs" in node):
-        for n, i in zip(node["childs"], range(0, len(node["childs"]))):
-            print_url_tree(n, f"{number}.{i+1}", level+1)
+    if(include_params):
+        print_params(current_node, identation=identation)
 
-def print_params():
-    target_url = search_tree(input("Target URL: "), url_tree)
-    for param in target_url["params"]:
-        print(param)
+    if("childs" in current_node):
+        for child_node, index in zip(current_node["childs"], range(0, len(current_node["childs"]))):
+            print_url_tree(child_node, f"{node_number}.{index+1}", level+1, include_params=include_params)
+
+def print_params(target_url=False, identation=""):
+    if(not target_url):
+        target_url = search_tree(input("Target URL: "), url_tree)
+
+    if("params" in target_url):    
+        for param in target_url["params"]:
+            print(f"{identation} {param}")
+
+def print_urls_params():
+    print_url_tree(include_params=True)
 
 def search_tree(number, node):
-    split_number = number.split(".")
-    split_number.pop(0)
-    if(len(split_number) == 0):
+    index_array = number.split(".")
+    index_array.pop(0)
+    
+    if(len(index_array) == 0):
         return node
+    
     else:
-        next_number = split_number[0]
-        return search_tree(".".join(split_number), node["childs"][int(next_number) - 1])
+        next_index = int(index_array[0]) - 1
+        next_node = node["childs"][next_index]
+
+        return search_tree(".".join(index_array), next_node)
         
 
 if __name__ == "__main__":
