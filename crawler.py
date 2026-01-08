a1e6676603523c9ac1ec56916feb50b88694945a
Davi <davirdavanzo@gmail.com>
2026-01-08 16:10:55 -0300

    Refactoring

--- crawler.py
+++ crawler.py
@@ -18,68 +18,49 @@ def crawl(url, cookies, url_tree, domain=""):
         return child_urls
 
 
-    links = re.findall(r'(?:href|src)="(.*?)"', response.text)
+    page_links = re.findall(r'(?:href|src)="(.*?)"', response.text)
 
-    links = list(set(links))
+    page_links = list(set(page_links))
 
-    for link in links:
+    for link in page_links:
         split_url = link.split("?", 1)
+
         child_url = split_url[0]
 
-        parsed = urlparse(child_url)
+        child_url = child_url.rstrip("/")
 
+        query_params = split_url[1].split("&") if len(split_url) > 1 else []
+
+        parsed = urlparse(child_url)
+    
         if parsed.scheme not in ("http", "https", ""):
             continue 
 
         if not parsed.scheme:
             child_url = urljoin(url, child_url)
 
-        child_url = child_url.rstrip("/")
 
-        query_params = split_url[1].split("&") if len(split_url) > 1 else []
-
-        existing_node = find_node_by_url(url=child_url, node=url_tree)
-
-        if(existing_node is not None):
-            existing_node['params'].extend(query_params)
-            existing_node['params'] = list(set(existing_node['params']))
+        if not check_valid_url(new_url=child_url,
+                               new_nodes=child_urls,
+                               domain=domain,
+                               url_tree=url_tree,
+                               query_params=query_params
+                            ):
             continue
 
-        if not (check_domain(url=child_url, domain=domain)):
-            continue
-
-        print()
-
-        print(f"------Child URL:------")
-        print(child_url)
-        print(f"----------------------")
+        print(f"\nURL found: {child_url}")
 
         if(len(query_params) > 1):
-            print("------Params------")
-            for param in query_params:
-                print(param)
-            print("------------------")
+            print(f"Params: {query_params}")
 
-        print()
-
-        url_exists = False
-
-        for existing_child in child_urls:
-            if(existing_child['url'] == child_url):
-                existing_child['params'].extend(query_params)
-                existing_child['params'] = list(set(existing_child['params']))
-                url_exists = True
-                break
-        if(url_exists):
-            continue
 
         child_urls.append({"url":child_url, "params":query_params})
 
 
     return child_urls
 
-def crawl_one(target_url, cookies, url_tree, domain=""):
-    target_url["childs"] = crawl(target_url["url"], cookies, url_tree=url_tree, domain=domain)
+def crawl_one(node, cookies, url_tree, domain=""):
+    node["childs"] = crawl(node["url"], cookies, url_tree=url_tree, domain=domain)
 
 def crawl_all(node, cookies, url_tree, domain='', visited=None):
 
@@ -102,3 +83,21 @@ def check_domain(url, domain):
 
     return new_url_domain == domain or new_url_domain.endswith(f'.{domain}') or domain == ''
 
+def check_valid_url(new_url, new_nodes, domain, url_tree, query_params):
+    if not (check_domain(url=new_url, domain=domain)):
+        return False
+
+    for node in new_nodes:
+        if(node['url'] == new_url):
+            node['params'].extend(query_params)
+            node['params'] = list(set(node['params']))
+            return False
+
+    existing_node = find_node_by_url(url=new_url, node=url_tree)
+
+    if(existing_node is not None):
+        existing_node['params'].extend(query_params)
+        existing_node['params'] = list(set(existing_node['params']))
+        return False
+        
+    return True
